3cf44ef1283e5d4a931a1cdc330c73fd56ce1599
Luan Braz <luan_silvacpv@hotmail.com>
2023-04-03 10:33:54 -0300

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { NavComponent } from "./components/nav/nav.component";
 import { HomeComponent } from "./components/home/home.component";
 import { TecnicoListComponent } from "./components/tecnico/tecnico-list/tecnico-list.component";
+import { LoginComponent } from "./components/login/login.component";
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
       { path: "home", component: HomeComponent },
       { path: "tecnicos", component: TecnicoListComponent },
     ],
+  },
+  {
+    path: "login",
+    component: LoginComponent,
   },
 ];
 
