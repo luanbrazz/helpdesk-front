a1387665278791bf5083fc21aa912ecad121c142
Luan Braz <luan_silvacpv@hotmail.com>
2023-04-08 18:23:54 -0300

    .

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
 import { Router } from "@angular/router";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
   selector: "app-nav",
@@ -7,9 +9,31 @@ import { Router } from "@angular/router";
   styleUrls: ["./nav.component.css"],
 })
 export class NavComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
-    this.router.navigate(["tecnicos"]);
+    this.router.navigate(["home"]);
+  }
+
+  logout() {
+    const snackBarRef = this.snackBar.open(
+      "Deseja realmente sair do sistema?",
+      "Sim",
+      {
+        duration: 6000,
+        verticalPosition: "top",
+        horizontalPosition: "end",
+        panelClass: ["logout-snackbar"],
+      }
+    );
+
+    snackBarRef.onAction().subscribe(() => {
+      this.authService.logout();
+      this.router.navigate(["login"]);
+    });
   }
 }
