a6d5fe7c29354aa2aef32f31ab13a7ff0555d393
Luan Braz <luan_silvacpv@hotmail.com>
2023-04-08 16:10:48 -0300

    serviço de mensagens

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
+import { MatDialog } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
 import { Credenciais } from "src/app/models/credenciais";
 
 @Component({
@@ -16,11 +18,24 @@ export class LoginComponent implements OnInit {
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
 
-  constructor() {}
+  constructor(private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {}
 
-  logar() {}
+  logar() {
+    if (this.validaCampos()) {
+      // lógica de login aqui
+    } else {
+      this.creds.senha = "";
+
+      this.snackBar.open("Usuário ou senha inválida!", "Fechar", {
+        duration: 4000, // duração da exibição em milissegundos
+        verticalPosition: "top", // posição vertical do popup
+        horizontalPosition: "end", // posição horizontal do popup
+        panelClass: ["custom-snackbar"], // classe CSS personalizada para o popup
+      });
+    }
+  }
 
   validaCampos(): boolean {
     if (this.email.valid && this.senha.valid) {
