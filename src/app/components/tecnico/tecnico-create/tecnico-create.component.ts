bca3e6b240c4b5a0c88ea4876ca3d7496aecb1f9
Luan Braz <luan_silvacpv@hotmail.com>
2023-04-10 08:10:32 -0300

    .

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-tecnico-create',
-  templateUrl: './tecnico-create.component.html',
-  styleUrls: ['./tecnico-create.component.css']
+  selector: "app-tecnico-create",
+  templateUrl: "./tecnico-create.component.html",
+  styleUrls: ["./tecnico-create.component.css"],
 })
 export class TecnicoCreateComponent implements OnInit {
+  @ViewChild("passwordInput") passwordInput: any;
+  hide = true;
 
-  constructor() { }
+  currentDate: Date;
+  constructor() {}
 
   ngOnInit(): void {
+    this.dataHora();
   }
 
+  togglePasswordVisibility(): void {
+    this.hide = !this.hide;
+    if (!this.hide) {
+      setTimeout(() => {
+        this.hide = true;
+        this.passwordInput.nativeElement.type = "password";
+      }, 2000);
+    }
+  }
+
+  dataHora() {
+    setInterval(() => {
+      this.currentDate = new Date();
+    }, 1000);
+  }
 }
