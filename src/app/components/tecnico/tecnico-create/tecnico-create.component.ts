5be01b7cd5dda487ccb82fbf14ee0ae40456bbea
Luan Braz <luan_silvacpv@hotmail.com>
2023-04-15 16:04:59 -0300

    tecnico create

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Router } from "@angular/router";
+import { Tecnico } from "src/app/models/tecnico";
+import { TecnicoService } from "src/app/services/tecnico.service";
 
 @Component({
   selector: "app-tecnico-create",
@@ -10,16 +14,74 @@ export class TecnicoCreateComponent implements OnInit {
   @ViewChild("passwordInput") passwordInput: any;
   hide = true;
 
+  currentDate: Date;
+
+  tecnico: Tecnico = {
+    id: "",
+    nome: "",
+    cpf: "",
+    email: "",
+    senha: "",
+    perfis: [],
+    dataCriacao: "",
+  };
+
   nome: FormControl = new FormControl(null, Validators.minLength(4));
   cpf: FormControl = new FormControl(null, Validators.required);
   email: FormControl = new FormControl(null, Validators.email);
   senha: FormControl = new FormControl(null, Validators.minLength(4));
 
-  currentDate: Date;
-  constructor() {}
+  constructor(
+    private service: TecnicoService,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.dataHora();
+  }
+
+  create(): void {
+    this.service.create(this.tecnico).subscribe({
+      next: (resposta) => {
+        this.snackBar.open("Técnico cadastrado com sucesso!", "Fechar", {
+          duration: 4000,
+          verticalPosition: "top",
+          horizontalPosition: "end",
+          panelClass: ["success-message"],
+        });
+        this.router.navigate(["/tecnicos"]);
+      },
+      error: (ex) => {
+        console.log(ex);
+
+        if (ex.error.errors) {
+          ex.error.errors.forEach((element) => {
+            this.snackBar.open(element.message, "Fechar", {
+              duration: 4000,
+              verticalPosition: "top",
+              horizontalPosition: "end",
+              panelClass: ["error-message"],
+            });
+          });
+        } else {
+          this.snackBar.open(ex.error.message, "Fechar", {
+            duration: 4000,
+            verticalPosition: "top",
+            horizontalPosition: "end",
+            panelClass: ["error-message"],
+          });
+        }
+      },
+    });
+  }
+
+  addPerfil(perfil: any) {
+    if (this.tecnico.perfis.includes(perfil)) {
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
+    } else {
+      this.tecnico.perfis.push(perfil);
+    }
   }
 
   validaCampos(): boolean {
