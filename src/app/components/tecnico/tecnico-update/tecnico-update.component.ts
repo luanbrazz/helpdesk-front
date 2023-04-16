cadcf47ba0f35d7d4ea5a3463c3f42bb7dbb97fe
Luan Braz <luan_silvacpv@hotmail.com>
2023-04-16 13:24:12 -0300

    update

---
+++ src/app/components/tecnico/tecnico-update/tecnico-update.component.ts
@@ -0,0 +1,121 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Tecnico } from "src/app/models/tecnico";
+import { TecnicoService } from "src/app/services/tecnico.service";
+
+@Component({
+  selector: "app-tecnico-update",
+  templateUrl: "./tecnico-update.component.html",
+  styleUrls: ["./tecnico-update.component.css"],
+})
+export class TecnicoUpdateComponent implements OnInit {
+  @ViewChild("passwordInput") passwordInput: any;
+  hide = true;
+
+  currentDate: Date;
+
+  tecnico: Tecnico = {
+    id: "",
+    nome: "",
+    cpf: "",
+    email: "",
+    senha: "",
+    perfis: [],
+    dataCriacao: "",
+  };
+
+  nome: FormControl = new FormControl(null, Validators.minLength(4));
+  cpf: FormControl = new FormControl(null, Validators.required);
+  email: FormControl = new FormControl(null, Validators.email);
+  senha: FormControl = new FormControl(null, Validators.minLength(4));
+
+  constructor(
+    private service: TecnicoService,
+    private snackBar: MatSnackBar,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.dataHora();
+    this.pegarIdDaUrl();
+    this.findById();
+  }
+
+  pegarIdDaUrl() {
+    this.tecnico.id = this.route.snapshot.paramMap.get("id");
+  }
+
+  findById(): void {
+    this.service.findById(this.tecnico.id).subscribe((resp) => {
+      this.tecnico = resp;
+    });
+  }
+
+  update(): void {
+    this.service.update(this.tecnico).subscribe({
+      next: (resposta) => {
+        this.snackBar.open("Técnico atualizado com sucesso!", "Fechar", {
+          duration: 4000,
+          verticalPosition: "top",
+          horizontalPosition: "end",
+          panelClass: ["success-message"],
+        });
+        this.router.navigate(["/tecnicos"]);
+      },
+      error: (ex) => {
+        console.log(ex);
+
+        if (ex.error.errors) {
+          ex.error.errors.forEach((element) => {
+            this.snackBar.open(element.message, "Fechar", {
+              duration: 4000,
+              verticalPosition: "top",
+              horizontalPosition: "end",
+              panelClass: ["error-message"],
+            });
+          });
+        } else {
+          this.snackBar.open(ex.error.message, "Fechar", {
+            duration: 4000,
+            verticalPosition: "top",
+            horizontalPosition: "end",
+            panelClass: ["error-message"],
+          });
+        }
+      },
+    });
+  }
+
+  addPerfil(perfil: any) {
+    if (this.tecnico.perfis.includes(perfil)) {
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
+    } else {
+      this.tecnico.perfis.push(perfil);
+    }
+  }
+
+  validaCampos(): boolean {
+    return (
+      this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid
+    );
+  }
+
+  togglePasswordVisibility(): void {
+    this.hide = !this.hide;
+    if (!this.hide) {
+      setTimeout(() => {
+        this.hide = true;
+        this.passwordInput.nativeElement.type = "password";
+      }, 2000);
+    }
+  }
+
+  dataHora() {
+    setInterval(() => {
+      this.currentDate = new Date();
+    }, 1000);
+  }
+}
