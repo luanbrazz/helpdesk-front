80360e5d871b8c1c5d73fbf2dfe81e18f872a6e3
Luan Braz <luan_silvacpv@hotmail.com>
2023-04-21 20:18:58 -0300

    create chamado

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-chamado-create',
-  templateUrl: './chamado-create.component.html',
-  styleUrls: ['./chamado-create.component.css']
+  selector: "app-chamado-create",
+  templateUrl: "./chamado-create.component.html",
+  styleUrls: ["./chamado-create.component.css"],
 })
 export class ChamadoCreateComponent implements OnInit {
+  currentDate: Date;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.dataHora();
   }
 
+  dataHora() {
+    setInterval(() => {
+      this.currentDate = new Date();
+    }, 1000);
+  }
 }
