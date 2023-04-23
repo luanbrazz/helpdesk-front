d93871edfccf725fb25366216f1e5ab5e6e09d8a
Luan Braz <luan_silvacpv@hotmail.com>
2023-04-22 23:53:31 -0300

    componente chamado-read criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,6 +47,7 @@ import { ClienteDeleteComponent } from "./components/cliente/cliente-delete/clie
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
+import { ChamadoReadComponent } from './components/chamado/chamado-read/chamado-read.component';
 
 @NgModule({
   declarations: [
@@ -69,6 +70,7 @@ import { ChamadoUpdateComponent } from './components/chamado/chamado-update/cham
     ChamadoListComponent,
     ChamadoCreateComponent,
     ChamadoUpdateComponent,
+    ChamadoReadComponent,
   ],
   imports: [
     BrowserModule,
